cbcd9ba0239d6b3020739cb44e7b153017215649
kasidis-manasurangkul <84066800+kasidis-manasurangkul@users.noreply.github.com>
2023-07-17 21:51:08 +0700

    add doc

--- viriyah_static_web/src/app/health-and-accident/health/health.component.ts
+++ viriyah_static_web/src/app/health-and-accident/health/health.component.ts
@@ -2,60 +2,68 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
-  selector: 'app-health',
-  templateUrl: './health.component.html',
-  styleUrls: ['./health.component.css']
+    selector: 'app-health',
+    templateUrl: './health.component.html',
+    styleUrls: ['./health.component.css']
 })
 export class HealthComponent implements OnInit {
-  public type: any;
-  fileNames: Array<number> = [1,2,3,4,5];
-  folder: String = '';
-  title: String = '';
+    public type: any;
+    fileNames: Array<number> = [1, 2, 3, 4, 5];
+    folder: String = '';
+    title: String = '';
 
-  constructor(private route: ActivatedRoute){
-    this.route.params.subscribe(
-      (params:Params)=>{
-          this.type = params['type'];
-          this.chooseType()
+    constructor(private route: ActivatedRoute) {
+        this.route.params.subscribe(
+            (params: Params) => {
+                this.type = params['type'];
+                this.chooseType()
+            }
+        )
+    }
+
+
+    ngOnInit(): void {
+        this.chooseType()
+    }
+
+    chooseType(): void {
+        if (this.type == 'vBetter') {
+            this.folder = 'v-better'
+            this.fileNames = [1, 2, 3, 4, 5, 6];
+            this.title = 'V Better Care'
         }
-    )}
-  
+        else if (this.type == 'vPrestige') {
+            this.folder = 'v-prestige'
+            this.fileNames = [1, 2, 3, 4, 5, 6];
+            this.title = 'V Prestige Care'
+        }
+        else if (this.type == 'vDelight') {
+            this.folder = 'v-delight'
+            this.fileNames = [1, 2, 3, 4, 5];
+            this.title = 'V-Delight Care'
+        }
+        else if (this.type == 'vDeluxe') {
+            this.folder = 'v-deluxe'
+            this.fileNames = [1, 2, 3, 4, 5];
+            this.title = 'V-Deluxe Care'
+        }
+        else if (this.type == 'vClassic') {
+            this.folder = 'v-classic'
+            this.fileNames = [1, 2, 3, 4, 5, 6];
+            this.title = 'Viriyah Classic'
+        }
+        else if (this.type == 'vGold') {
+            this.folder = 'v-gold'
+            this.fileNames = [1, 2, 3, 4, 5, 6];
+            this.title = 'Viriyah Gold'
+        }
+        else if (this.type == 'vHealthCare') {
+            this.folder = 'v-health-care'
+            this.fileNames = [1, 2, 3, 4, 5];
+            this.title = 'Viriyah Healthcare'
+        }
 
-  ngOnInit(): void {
-    this.chooseType()
-  }
 
-  chooseType(): void{
-    if (this.type == 'vBetter'){
-      this.folder = 'v-better'
-      this.title = 'V Better Care'
     }
-    else if (this.type == 'vPrestige'){
-      this.folder = 'v-prestige'
-      this.title = 'V Prestige Care'
-    }
-    else if (this.type == 'vDelight'){
-      this.folder = 'v-delight'
-      this.title = 'V-Delight Care'
-    }
-    else if (this.type == 'vDeluxe'){
-      this.folder = 'v-deluxe'
-      this.title = 'V-Deluxe Care'
-    }
-    else if (this.type == 'vClassic'){
-      this.folder = 'v-classic'
-      this.title = 'V Better Care'
-    }
-    else if (this.type == 'vGold'){
-      this.folder = 'v-gold'
-      this.title = 'V Better Care'
-    }
-    else if (this.type == 'vHealthCare'){
-      this.folder = 'v-health-care'
-      this.title = 'V Better Care'
-    }
-    
-    
-  }
 
 }
