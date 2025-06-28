876df8a9a177b669310644de2cc1c58f43139e2d
viriyahrama4 <viriyah.rama4@gmail.com>
2025-06-29 01:07:24 +0700

    upudate dowlnoad pdf of helath

--- src/app/health-and-accident/health/health.component.ts
+++ src/app/health-and-accident/health/health.component.ts
@@ -11,6 +11,7 @@ export class HealthComponent implements OnInit {
     fileNames: Array<number> = [1, 2, 3, 4, 5];
     folder: String = '';
     title: String = '';
+    hasForm: boolean = false;
 
     constructor(private route: ActivatedRoute) {
         this.route.params.subscribe(
@@ -31,46 +32,55 @@ export class HealthComponent implements OnInit {
             this.folder = 'v-better'
             this.fileNames = [1, 2, 3, 4, 5, 6];
             this.title = 'V Better Care'
+            this.hasForm = true;
         }
         else if (this.type == 'vPrestige') {
             this.folder = 'v-prestige'
             this.fileNames = [1, 2, 3, 4, 5, 6];
             this.title = 'V Prestige Care'
+            this.hasForm = true;
         }
         else if (this.type == 'vDelight') {
             this.folder = 'v-delight'
             this.fileNames = [1, 2, 3, 4, 5];
             this.title = 'V-Delight Care'
+            this.hasForm = true;
         }
         else if (this.type == 'vDeluxe') {
             this.folder = 'v-deluxe'
             this.fileNames = [1, 2, 3, 4, 5];
             this.title = 'V-Deluxe Care'
+            this.hasForm = true;
         }
         else if (this.type == 'vClassic') {
             this.folder = 'v-classic'
             this.fileNames = [1, 2, 3, 4, 5, 6];
             this.title = 'Viriyah Classic'
+            this.hasForm = true;
         }
         else if (this.type == 'vGold') {
             this.folder = 'v-gold'
             this.fileNames = [1, 2, 3, 4, 5, 6];
             this.title = 'Viriyah Gold'
+            this.hasForm = true;
         }
         else if (this.type == 'vHealthCare') {
             this.folder = 'v-health-care'
             this.fileNames = [1, 2, 3, 4, 5];
             this.title = 'Viriyah Healthcare'
+            this.hasForm = true;
         }
         else if (this.type == 'vCancerEasy') {
             this.folder = 'v-cancer-easy'
             this.fileNames = [1, 2, 3, 4];
             this.title = 'Viriyah Cancer Easy'
+            this.hasForm = true;
         }
         else if (this.type == 'vCancerPro') {
             this.folder = 'v-cancer-pro'
             this.fileNames = [1, 2, 3, 4];
             this.title = 'Viriyah Cancer Pro'
+            this.hasForm = true;
         }
 
 
